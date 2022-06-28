e894aff611a61f77e0cd299caa9c9a0ac46b0645
ppoliani <ppoliani@gmail.com>
2022-06-28 11:03:04 +0300

    feat: add the pallet config

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -4,7 +4,7 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
 	use frame_support::traits::{Currency, Randomness};
 
@@ -13,13 +13,27 @@ pub mod pallet {
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
 
-	// The Pallet's configuration trait.
-	// Here we include some custom configurations which will
-	// allow our pallet to gain access to outside interfaces like
+	/// The Pallet's configuration trait.
+	/// Here we include some custom configurations which will allow our pallet to gain access to outside interfaces like
+	/// This trait always inherits from the frame_system::Config which defines all the core types in your runtime, like:
+	/// - T::AccountId
+	/// - T::BlockNumber
+	/// These traits allows your runtime to be generic, which means that you can easily change the 
+	/// fundamental types in your blockchain, and things will still work.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		/// A trait that describes an interface to access and manipulate user balances.
+		/// Also gives you access to the Balance type.
+		type Currency: Currency<Self::AccountId>;
+
+		/// The max number of NFT a single account can own
+		#[pallet::constant]
+		type PersonalCap: Get<u32>;
+
+		type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
 	}
 
 	// Your Pallet's events.
